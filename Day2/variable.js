6e35c6dfc59034aeb05e4280b2b9237e225fa64e
Yaratch_Blake <Yaratch_Blake@student.mahoningctc.com>
2020-12-02 10:45:57 -0500

    Day 2

---
+++ Day2/variable.js
@@ -0,0 +1,36 @@
+let a;
+a=5;
+let b=10;
+answer=a+b;
+console.log(answer);
+// document.writeln(a+b);
+
+let td=6;
+let fg=1;
+let safty=2;
+let extraPoint=2;
+
+var score=0;
+// Duplicate line shortcut: Shit+Alt+Down Arrow
+// Multi select: Middle mouse and move curser 
+score= score+td
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+score+=td;
+
+
+answer++;
+
+document.getElementById("answer").innerHTML=score;
+
+
+var myString= "Howdy."
+document.getElementById("myParagraph").innerHTML=myString.split(".");
